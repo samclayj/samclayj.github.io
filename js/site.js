2580c5cd7233a4fa74af3d9273527e4cdc8fba08
Sam Jentsch <samj@Sams-MacBook-Pro.local>
2018-05-21 07:42:22 -0700

    Tuned pulse effect.

--- js/site.js
+++ js/site.js
@@ -1,10 +1,17 @@
 let appState = {
+    togglePressed: false,
     headerCollapsed: true,
 }
 
 let headerCollapse = function() {
+    // Stop pulsating since the toggle has been acknowledged.
+    appState.togglePressed = true;
+    let headerToggle = document.getElementById("header-toggle-icon");
+    headerToggle.setAttribute("pressed", appState.togglePressed);
+
     let header = document.getElementById("smallscreen-info-pane");
     header.setAttribute("show", appState.headerCollapsed);
+    header.setAttribute("pressed", appState.headerCollapsed);
     appState.headerCollapsed = !appState.headerCollapsed;
 }
 
